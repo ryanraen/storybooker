854dcfdb5fe2ce3ab669968c8cc5ccd86357259d
ryanraen <ryan.xie129@gmail.com>
2025-09-06 21:36:04 -0700

    updated routing

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -9,6 +9,7 @@ import Dashboard from './pages/Dashboard';
 import Generate from './pages/Generate'
 import History from './pages/History'
 import Examples from './pages/Examples'
+import Signup from './pages/Signup'
 import Login from './pages/Login'
 
 import homeTheme from './themes/homeTheme';
@@ -23,13 +24,14 @@ root.render(
             <BrowserRouter>
                 <Routes>
                     <Route path="/" element={<App />} /> 
+                    <Route path="signup" element={<Signup />} />
                     <Route path="login" element={<Login />} /> 
-                    <Route path="dashboard" element={<Dashboard />}>
+                    <Route path="dashboard/" element={<Dashboard />}>
                         <Route index element={<Generate />} />
                         <Route path="generate" element={<Generate />} />
                         <Route path="history" element={<History />} />
                     </Route>
-                    <Route path="/examples" element={<Examples />} />
+                    <Route path="examples" element={<Examples />} />
                 </Routes>
             </BrowserRouter>
         </ThemeProvider>
