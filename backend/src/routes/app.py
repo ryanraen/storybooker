2e40d89b05d85b3f859b5b6d35643d18523a50a5
ryanraen <ryan.xie129@gmail.com>
2025-09-13 00:17:46 -0700

    updated CORS origin

--- backend/src/routes/app.py
+++ backend/src/routes/app.py
@@ -10,7 +10,7 @@ from base64 import b64encode
 
 def create_app():
     app = Flask(__name__)
-    cors = CORS(app, resources={r"/*": {"origins": "https://storybooker.vercel.app/"}})
+    cors = CORS(app, resources={r"/*": {"origins": "https://storybooker.vercel.app"}})
 
     dotenv.load_dotenv()
 
